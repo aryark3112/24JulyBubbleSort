2917f68065ba3ebbf8a488d69af4e6c960cba7eb
aryark <aryark3112@gmail.com>
2023-07-24 10:42:49 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+            for(int i=0;i<n-1; i++)
+            {
+                swapped = false;
+                for(int j = 0; j<n-i-1;j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                { break; }
+            }
+        }
+        public static void Print(int[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]+" ");
+            }
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            Console.WriteLine("Print Array without Sort");
+            Print(arr);
+            BubbleSort(arr);
+            Console.WriteLine("After Bubble Sort");
+            Print(arr);
+            Console.ReadKey();
+        }
+    }
+}
